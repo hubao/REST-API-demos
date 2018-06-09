6a284fd72609008dfbc3fa9e568daa948cf13821
hubao <cmwl98@hotmail.com>
2018-06-09 15:38:13 +0800

    go学习

--- REST-GO-demo/main.go
+++ REST-GO-demo/main.go
@@ -19,6 +19,24 @@ func main() {
 	 * dir in Code
 	 */
 
-	 fmt.Printf("测试")
+	 fmt.Printf("测试\n")
+
+	 // 创建数组
+	 var myArray [10]int = [10]int{1,2,3,4,5,6,7,8,9,10}
+
+	 // 基于数组创建数组切片
+	 var mySlice []int = myArray[:4]
+
+	 fmt.Println("Elements of myArray: ")
+	 for _,v := range myArray{
+	 	fmt.Print(v, " ")
+	 }
+
+	 fmt.Println("\nElements of mySlice: ")
+	 for _,v := range mySlice{
+	 	fmt.Print(v, " ")
+	 }
+
+	 fmt.Println(cap(mySlice))
 }
 
